a0a56b93fe6b6e5b7ae811d51800d2d552bb3005
Artoshi <teerapat15.15@gmail.com>
2022-11-15 16:13:40 +0700

    Main

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 require("colors");
 
 //connect DB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/apitest");
+    const conn = await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/apitest"
+    );
     console.log(`MongoDB Connected Successfully!`.bgGreen.inverse);
   } catch (error) {
     console.log(`Error :${error.message}`.bgRed.inverse);
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+require("dotenv").config();
 require("colors");
 const connectDB = require("./config/config");
 
